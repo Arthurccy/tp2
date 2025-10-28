5a14838f9e2deaaaecfd809192657cfaa505a46e
Arthurccy <arthurcourchay@gmail.com>
2025-10-28 11:50:23 +0100

    CRUD articles

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,12 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from django.urls import path
+from articles.views import ArticleViewSet
 
 router = DefaultRouter()
-
+router.register(r'articles', ArticleViewSet, basename='article')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)), 
+    path('api/', include(router.urls)),
 ]
